ece266d8fb0df4174f12b288e672f284a6e9ab26
superkevin@gmail.com <superkevin@gmail.com>
2013-07-05 16:09:00 -0400

    some stuff works

--- main.js
+++ main.js
@@ -12,9 +12,36 @@ function textCallback(textResponse) {
 
 jQuery(function($) {
   var photos = $('#photos');
-  var fp = photoPosts[0];
-  var photoSrc = fp['photo-url-500'];
-  var photoUrl = fp['url-with-slug'];
-  var photoEl = $('<a href="' + photoUrl + '"><img src="' + photoSrc + '"></a>');
-  photos.append(photoEl);
+  function addPhoto(photo, url) {
+    var photoSrc = photo['photo-url-500'];
+    var photoEl = $('<a href="' + url + '"><img src="' + photoSrc + '"></a>');
+    photos.append(photoEl);
+  }
+  photoPosts.forEach(function(fp) {
+    var postUrl = fp['url-with-slug'];
+    if (fp['photos']) {
+      fp['photos'].forEach(function(photo) {
+        addPhoto(photo, postUrl);
+      });
+    } else {
+      addPhoto(fp, postUrl);
+    }
+  });
+  photos.slidesjs({width:640, height:480, play:{
+    active: false,
+    effect: "fade",
+    interval: 5000,
+    auto: true,
+    swap: true,
+    pauseOnHover: true,
+    restartDelay: 2500
+  }});
+  
+  var texts = $('#texts');
+  var ft = textPosts[0];
+  console.log(ft);
+  var title = ft['regular-title'];
+  var body = ft['regular-body'];
+  texts.append($('<h1>' + title + "</h1>"))
+    .append($(body));
 });
