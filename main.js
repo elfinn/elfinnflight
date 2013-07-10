598cfc82ed33583627d8a5beb3c7bd7ebffaf2f9
superkevin@gmail.com <superkevin@gmail.com>
2013-07-10 17:59:37 -0400

    changed main.js into a script included separately

---
+++ main.js
@@ -0,0 +1,47 @@
+var photoPosts;
+function photosCallback(photoResponse) {
+  photoPosts = photoResponse['posts'];
+};
+
+var textPosts;
+function textCallback(textResponse) {
+  textPosts = textResponse['posts'];
+};
+
+jQuery(function($) {
+  var photos = $('#photos');
+  function addPhoto(photo) {
+    var photoSrc = photo['photo-url-500'];
+    var photoEl = $('<a href="//elfinnflight.com/tagged/photos"><img src="' + photoSrc + '"></a>');
+    photos.append(photoEl);
+  }
+  photoPosts.forEach(function(fp) {
+    // var postUrl = fp['url-with-slug'];
+    if (fp['photos']) {
+      fp['photos'].forEach(function(photo) {
+        addPhoto(photo);
+      });
+    } else {
+      addPhoto(fp);
+    }
+  });
+  photos.slidesjs({
+    width: 500,
+    height: 375,
+    play: {
+      active: false,
+      effect: "fade",
+      auto: true,
+      pauseOnHover: true,
+    },
+    navigation: {
+      active: false
+    }});
+  
+  var texts = $('#texts');
+  var ft = textPosts[0];
+  var title = ft['regular-title'];
+  var body = ft['regular-body'];
+  texts.append($('<h1>' + title + "</h1>"))
+    .append($(body));
+});
